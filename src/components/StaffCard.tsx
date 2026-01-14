32fe75ae57101b103b6a8f183cfa19b671c823a6
ShikiHTM <kizukusan123@gmail.com>
2026-01-15 02:35:24 +0700

    Remove scale animation

--- src/components/StaffCard.tsx
+++ src/components/StaffCard.tsx
@@ -10,16 +10,8 @@ type StaffCardProps = {
 
 const StaffCard: React.FC<StaffCardProps> = ({ name, role, avatarUrl, bannerUrl }) => {
 
-  /*
-      TODO:
-      - Fix font issues
-      - Fix avatar position
-      - Add hover effects (done)
-   */
-
   return (
-    <div className="container relative hover:scale-105 transition-transform duration-300 ease-in-out">
-      
+    <div className="container relative transition-all duration-300 hover:brightness-110 hover:saturate-110">
       <div className='relative flex shrink-0 items-center p-1'>
         {/* Avatar and Info */}
         <div className="bg-white relative w-12 h-12 overflow-hidden rounded-l-lg">
@@ -27,26 +19,38 @@ const StaffCard: React.FC<StaffCardProps> = ({ name, role, avatarUrl, bannerUrl 
             src={avatarUrl}
             alt={`${name} Avatar`}
             fill
-            className='object-contain'
+            className='object-cover'
           />
         </div>
 
         {/* Banner */}
-        <div className='relative flex flex-1 w-full h-12 overflow-hidden shadow-lg p-2'>
+        <div className='relative flex flex-1 w-full h-12 overflow-hidden shadow-lg p-2 rounded-r-lg'>
           <Image
             src={bannerUrl}
             alt={`${name} Banner`}
             fill
-            className='object-cover rounded-r-lg'
+            className='object-cover'
           />
           
           {/* Gradient */}
           <div className="absolute inset-0 bg-gradient-to-r from-black/80 to-transparent"></div>
 
           {/* Name and Role */}
-          <div className="relative z-10 left-1.5 flex flex-col items-start justify-center">
-            <div className="text-2xl font-bold text-white">{name} </div>
-            <div className="text-sm">{role}</div>
+          <div className="font-sans relative z-10 left-1.5 flex flex-col items-start justify-center">
+            <div style={{
+              paddingBottom: '0.2rem',
+              fontSize: '1.15rem',
+              fontWeight: 600,
+              textShadow: '1px 1px 3px rgba(0, 0, 0, 0.7)',
+            }}>
+              {name}
+            </div>
+            <div style={{
+              fontSize: '0.875rem',
+              fontStyle: 'italic'
+            }}>
+              {role}
+            </div>
           </div>
         </div>
       </div>
