6d2540a7cdf1cc7e2fb77b9a41509f9a1e95f641
ShikiHTM <kizukusan123@gmail.com>
2026-01-14 16:46:14 +0700

    Refactor (Staff Card): Update layout and visual design

--- src/components/StaffCard.tsx
+++ src/components/StaffCard.tsx
@@ -18,31 +18,39 @@ const StaffCard: React.FC<StaffCardProps> = ({ name, role, avatarUrl, bannerUrl 
    */
 
   return (
-    <div className="border-2 border-amber-50 container relative hover:scale-105 transition-transform duration-300 ease-in-out">
-      {/* Banner */}
-      <div className='w-full h-12 relative rounded-lg overflow-hidden shadow-lg'>
-        <Image
-          src={bannerUrl}
-          alt={`${name} Banner`}
-          fill
-          className='object-cover object-center'
-        />
-
-        {/* Gradient */}
-        <div className="absolute inset-0 bg-gradient-to-r from-black via-black to-transparent"></div>
-
+    <div className="container relative hover:scale-105 transition-transform duration-300 ease-in-out" style={{
+      border: '2px solid white',
+      borderRadius: '15px',
+      overflow: 'hidden',
+      backgroundColor: 'white'
+    }}>
+      
+      <div className='relative flex shrink-0 items-center p-1'>
         {/* Avatar and Info */}
-        <div className='relative flex items-center gap-3'>
-          <div className="relative w-12 h-12">
-            <Image 
-              src={avatarUrl}
-              alt={`${name} Avatar`}
-              fill
-            />
-          </div>
+        <div className="relative w-12 h-12 overflow-hidden">
+          <Image 
+            src={avatarUrl}
+            alt={`${name} Avatar`}
+            fill
+            className='object-contain'
+          />
+        </div>
 
-          <div className="relative flex flex-col justify-center items-start">
-            <div className="text-2xl font-bold text-white leading-tight">{name} </div>
+        {/* Banner */}
+        <div className='relative flex flex-1 w-full h-12 overflow-hidden shadow-lg p-2'>
+          <Image
+            src={bannerUrl}
+            alt={`${name} Banner`}
+            fill
+            className='object-cover'
+          />
+          
+          {/* Gradient */}
+          <div className="absolute inset-0 bg-gradient-to-r from-black/80 to-transparent"></div>
+
+          {/* Name and Role */}
+          <div className="relative z-10 left-1.5 flex flex-col items-start justify-center">
+            <div className="text-2xl font-bold text-white">{name} </div>
             <div className="text-sm">{role}</div>
           </div>
         </div>
